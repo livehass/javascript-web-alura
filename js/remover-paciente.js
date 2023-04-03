5bbae1f68cde17d6287a2885c99d522b17eb3fa0
livehass <silva.felipe12@hotmail.com>
2023-04-03 18:21:07 -0300

    some-changes

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -4,7 +4,10 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
  tabela.addEventListener("dblclick", function(event){
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode; //Tr = paciente = remover
 
+    paiDoAlvo.remove();
  })
 
 
