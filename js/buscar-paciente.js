42a0aa6e232c9e4b0ce38db9c717521b494104be
Felipe <silva.felipe12@hotmail.com>
2023-04-06 12:42:14 -0300

    Update buscar-paciente.js

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -5,7 +5,7 @@ botaoAdicionar.addEventListener("click", function(){
     //XMLHttpRequest requisição de dados no js.
     var xhr = new XMLHttpRequest();
     //função que abra a conexão que queremos fazer, tipo "get" para pegar o arquivo
-    xhr.open("get", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes1.json");
+    xhr.open("get", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
     //variavel para manipular o erro ao carregar clientes.
     var erroAjax = document.querySelector("#erro-ajax");
     // quando a "xhr" estiver carregada executa uma função
